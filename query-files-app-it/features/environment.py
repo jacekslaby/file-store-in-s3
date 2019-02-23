be5001db502f7ff7b6f85494b4e72f698f54ad3e
Jacek Slaby <jacek.slaby@interia.pl>
2019-02-23 14:59:29 +0000

    Added logic to create test objects. (i.e. new buckets and objects are created in s3)

--- query-files-app-it/features/environment.py
+++ query-files-app-it/features/environment.py
@@ -1,28 +1,51 @@
+import boto3
 
+# @TODO Read config settings from env variables.
 environment_name = 'it'
+region_name = 'Dummy'
 
+# Define test data to be created in s3.
+#
 # Let's provide domains names. Each with number of files.
 domains = {'Shell': 2, 'BP-upstream': 4, 'BP-midstream': 0, 'BP-downstream': 10, 'AL': 3, 'CA': 2}
-
+#
 # Let's create file names for every domain, e.g. 'Shell:0', 'Shell:1', etc.
 files_existing_in_domains = {domain_name: [domain_name + ':' + str(i) for i in range(file_count)]
                              for domain_name, file_count in domains.items()}
-
-# Let's provide buckets' names. Each with number of files.
-# <environmentName>::<grupa alos>::projSecret:_2345_345_333.
-# buckets_existing_in_s3 = {environment_name + '::' + domain_name + ':: @TODO unique UUID': file_count
-#                          for domain_name, file_count in domains.items()}
-
+#
 # Let's create bucket names, e.g. bucket 'it::Shell' with files ['Shell:0', 'Shell:1']
-files_existing_in_s3_buckets = {environment_name + '::' + domain_name + ':: @TODO unique UUID': files_list
+files_existing_in_s3_buckets = {environment_name + '--' + domain_name + '--unique-uuid': files_list
                                 for domain_name, files_list in files_existing_in_domains.items()}
 
 
 def before_all(context):
-    # @TODO Create all required files. (in s3 bucket)
-    # (it takes time so we want to do it once before all features/scenarios are run)
-    #  - key is domain name
-    #  - value is list of files existing in this domain
+    # Prepare s3 client.
+    # (based on:
+    #   https://realpython.com/python-boto3-aws-s3/
+    #   https://stackoverflow.com/questions/45981950/how-to-specify-credentials-when-connecting-to-boto3-s3
+    #   https://stackoverflow.com/questions/32618216/overwrite-s3-endpoint-using-boto3-configuration-file
+    #   https://stackoverflow.com/questions/48690698/using-boto3-through-sam-local-to-interact-with-localstack-s3
+    #   )
+    s3_resource = boto3.resource('s3', endpoint_url='http://localhost',
+                                 use_ssl=False,
+                                 region_name='Dummy',
+                                 aws_access_key_id='AccessKey',
+                                 aws_secret_access_key='SecertKey')
+    # Specify the region.
+    # (otherwise: "If you don't specify a region, the bucket will be created in US Standard." )
+    bucket_configuration = {'LocationConstraint': region_name}
+
+    # Create all required buckets and objects in s3, (it takes time so we do it before all features/scenarios are run)
+    # based on dictionary in which:
+    #  - key is bucket name
+    #  - value is list of object names in this bucket
+    #
+    for bucket_name, files_list in files_existing_in_s3_buckets.items():
+        # First create bucket.
+        s3_resource.create_bucket(Bucket=bucket_name.lower(), CreateBucketConfiguration=bucket_configuration)
+        # Then create all required objects. (in this bucket)
+        for object_name in files_list:
+            s3_resource.Object(bucket_name=bucket_name, key=object_name)
 
     # provide files in the scenario's context   (e.g. to check correctness of returned results)
     context.files_existing_in_domains = files_existing_in_domains
